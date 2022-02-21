9a50de3cfc24a6c222610a668546668687bdc81d
shrey2594 <shrey2510@gmail.com>
2022-02-21 22:02:32 +0530

    Snake and Ladder problem update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Problem");
+            Start.StartProblem();
         }
     }
 }
