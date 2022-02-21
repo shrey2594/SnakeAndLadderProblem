59580ba64068161ff33082ad6bf4af7acbf99c70
shrey2594 <shrey2510@gmail.com>
2022-02-21 22:12:41 +0530

    Roll the dice

---
+++ RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class RollDice
+    {
+        public static void Roll()
+        {
+            Random r = new Random();
+            int dice=r.Next(1,7);
+            Console.WriteLine(dice);
+        }
+    }
+}
