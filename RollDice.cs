56ee9d1ab2f3e03d0e860a0a3c8b0e8efb4bc8e2
shrey2594 <shrey2510@gmail.com>
2022-02-22 17:57:33 +0530

    Roll the dice

--- RollDice.cs
+++ RollDice.cs
@@ -8,11 +8,26 @@ namespace SnakeAndLadderProblem
 {
     public class RollDice
     {
-        public static void Roll()
+        public static void Roll(int start)
         {
+            int current = start;
             Random r = new Random();
             int dice=r.Next(1,7);
             Console.WriteLine(dice);
+            int option = CheckOption.Options();
+            Console.WriteLine(option);
+            if(option==1)
+            {
+                Console.WriteLine("currently player is at:" + (current+dice));
+            }
+            else if(option==2)
+            {
+                Console.WriteLine("Currently player is at:" + (current-dice)); ;
+            }
+            else
+            {
+                Console.WriteLine("Currently player is at:" + current);
+            }
         }
     }
 }
