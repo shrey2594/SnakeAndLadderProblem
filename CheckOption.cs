cd3b3a027a336ab63d1788f43c358cf863cfada4
shrey2594 <shrey2510@gmail.com>
2022-02-22 17:55:30 +0530

    Options to check

---
+++ CheckOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class CheckOption
+    {
+		public static int Options()
+		{
+			Random random = new Random();
+			int option = random.Next(0, 3);
+			switch (option)
+            {
+				case 0:return 0;
+				case 1:return 1;
+				case 2:return 2;
+				default:return 0;
+            }
+		}
+    }
+}
