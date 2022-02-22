70b0883e35e6ee5abe2965615098ff8df9524b51
shrey2594 <shrey2510@gmail.com>
2022-02-22 18:27:39 +0530

    Options to check

--- CheckOption.cs
+++ CheckOption.cs
@@ -8,15 +8,15 @@ namespace SnakeAndLadderProblem
 {
     public class CheckOption
     {
-		public static int Options()
+		public static int Options(int roll)
 		{
 			Random random = new Random();
 			int option = random.Next(0, 3);
 			switch (option)
             {
 				case 0:return 0;
-				case 1:return 1;
-				case 2:return 2;
+				case 1:return roll;
+				case 2:return 0-roll;
 				default:return 0;
             }
 		}
